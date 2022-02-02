c4fcccadc49c25e5d632cda30c496608bfc1857c
lm2612 <lauraman@stanford.edu>
2022-02-02 12:31:21 -0800

    test plotting zonal means

---
+++ Scripts/SavePlots_ubar.py
@@ -0,0 +1,45 @@
+### Example script to save ubar plots to file
+### Run as main from parent directory
+### python -m Scripts.SavePlots_ubar
+
+import os
+import glob
+import netCDF4 as nc
+import numpy as np
+import matplotlib.pyplot as plt
+
+from plot_functions.plot_ubar_seasonal import plot_ubar_seasonal
+from plot_functions.plot_ubar_annual import plot_ubar_annual
+from plot_functions.plot_ubar_daily import plot_ubar_daily
+from plot_functions.make_gif import make_gif
+
+# Select run to plot
+run = '038'
+basedir = os.environ['SCRATCH']+'/MiMA/runs/'
+rundir = basedir + run + '/'
+print(rundir)
+
+print(glob.glob(rundir+'*.nc'))
+filename = 'atmos_daily'
+dataset = nc.Dataset(rundir+filename+'.nc', 'r')
+
+lon = dataset['lon']
+lat = dataset['lat']
+time = dataset['time']
+pfull = dataset['pfull']
+ucomp = dataset['ucomp']
+
+# If plot folder does not exist, create one
+if not os.path.exists(rundir+'PLOTS/'):
+    os.makedirs(rundir+'PLOTS/')
+    print('New dir created: '+rundir+'PLOTS/')
+
+print("Plotting...")
+plot_ubar_annual(lat, pfull, ucomp, rundir)
+print("Annual plot done")
+plot_ubar_seasonal(lat, pfull, ucomp, rundir)
+print("Seasonal plot done")
+plot_ubar_daily(lat, pfull, ucomp, rundir, dday=15)
+print("Daily plots done")
+make_gif(rundir+'PLOTS/ubar_t')
+print("Gif created")
---
+++ Scripts/__init__.py
@@ -0,0 +1,2 @@
+## INIT FILE ##
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
