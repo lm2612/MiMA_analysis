3fcb698682f3ce342f11b7fd1bbbc933446d7ce9
lm2612 <lauraman@stanford.edu>
2022-02-01 16:54:34 -0800

    rename

--- plot_functions/gif_maker.py
+++
@@ -1,8 +0,0 @@
-import glob
-from PIL import Image
-def make_gif(path_to_images):
-    frames = [Image.open(image) for image in sorted(glob.glob(f"{path_to_images}*.png"))]
-    frame_one = frames[0]
-    frame_one.save("{}_animation.gif".format(path_to_images), format="GIF", append_images=frames,
-               save_all=True, duration=100, loop=0)
-    
