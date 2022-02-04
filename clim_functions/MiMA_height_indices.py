f3fe1f2211a718b5126d171affd454e6b26652dc
lm2612 <lauraman@stanford.edu>
2022-02-04 13:04:16 -0800

    referenceheight levels saved

---
+++ clim_functions/MiMA_height_indices.py
@@ -0,0 +1,10 @@
+"""For reference, the approx. model indices needed to get given height level in atmosphere """
+MiMA_height_indices = {'10hPa': 13,
+                       '20hPa': 16,
+                       '27hPa': 17,
+                       '40hPa': 19,
+                       '77hPa': 22,
+                       '100hPa':23,
+                       '200hPa':27,
+                       '850hPa':37}
+
