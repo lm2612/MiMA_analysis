cf4203c185b003dd728bb0142f9dc2efd67b499c
lm2612 <lauraman@stanford.edu>
2022-02-04 16:58:34 -0800

    fixed bug: QBO amp is calculated as avg over max of each PHASE not each CYCLE, i.e. 1/2 (absmax + absmin) = 1/2(max - min) since min is always negative

--- QBO_metrics/get_QBO_TT_metrics.py
+++ QBO_metrics/get_QBO_TT_metrics.py
@@ -7,7 +7,7 @@ from clim_functions.smoothing import smooth
 def get_QBO_TT(u_zonal, return_variance=False, return_amplitude=False, return_cov=False):
     """ Function that returns QBO period using Transition Time (TT) method 
     Inputs: u_zonal (np array) zonal mean zonal wind at given height level, recommended 10hPa (MiMA index 13) 
-    Outputs: period (flt) mean period in years 
+    Outputs: period (flt) mean period in months 
              amplitude (flt)  absolute max. u within each QBO cycle"""
     # Smooth with five-month centered running mean
     u_smoothed = smooth(u_zonal)
@@ -25,34 +25,35 @@ def get_QBO_TT(u_zonal, return_variance=False, return_amplitude=False, return_co
     print("QBO transition times:", new_QBO_cycle)
     new_QBO_cycle = np.array(new_QBO_cycle)
     periods = new_QBO_cycle[1:] - new_QBO_cycle[:-1]
-    periods_yr = periods/360.
+    periods_mon = periods/30.
     
     # Calculate mean period
-    mean_period = np.mean(periods_yr)
+    mean_period = np.mean(periods_mon)
     
     if return_amplitude:
         amplitudes = np.zeros(len(periods))
         for ic in range(len(periods)):
             start_ind = new_QBO_cycle[ic]
             end_ind = new_QBO_cycle[ic+1]
-            amplitudes[ic] = np.max(np.abs( u_smoothed[start_ind:end_ind] ))
+            amplitudes[ic] = 0.5 * (np.max(u_smoothed[start_ind:end_ind] ) - 
+                                    np.min(u_smoothed[start_ind:end_ind] ) )
 
         # Calculate mean amplitude
         mean_amplitude = np.mean(amplitudes)
         
         # And covariance or variance if required
         if return_cov:
-            cov = np.cov(periods_yr, amplitudes)
+            cov = np.cov(periods_mon, amplitudes)
             return mean_period, mean_amplitude, cov
         elif return_variance:
             var_amplitude = np.var(amplitudes)
-            var_period = np.var(periods_yr)
+            var_period = np.var(periods_mon)
             return mean_period, mean_amplitude, var_period, var_amplitude
         else:
             return mean_period, mean_amplitude
 
     elif return_variance:
-        var_period = np.var(periods_yr)
+        var_period = np.var(periods_mon)
         return mean_period, var_period
     else:
         return mean_period
