41ab2ad1fa5ea78e6e87d9858842e19b55f02565
lm2612 <lauraman@stanford.edu>
2022-02-04 16:58:55 -0800

    generalises to more dims by using axis=0

--- QBO_metrics/get_QBO_period_FFT.py
+++ QBO_metrics/get_QBO_period_FFT.py
@@ -1,7 +1,6 @@
 # Function to return period of QBO using FFT method
 import numpy as np
 import netCDF4 as nc
-from scipy import signal
 from scipy.fft import fft, fftfreq
 
 
@@ -13,15 +12,15 @@ def get_QBO_period_FFT(u_zonal):
     """ 
     # Calculate period
     # Remove mean
-    u_zonal_demean = u_zonal - np.mean(u_zonal)
+    u_zonal_demean = u_zonal - np.mean(u_zonal, axis=0)
     # Increase spectral resolution by padding edges
     pad = np.zeros(u_zonal.shape)
     u_zonal_padded = np.concatenate((pad, u_zonal_demean, pad), axis=0)
     # fft
-    power_spec = np.abs(fft(u_zonal_padded))**2
+    power_spec = np.abs(fft(u_zonal_padded, axis=0))**2
     freqs = fftfreq(len(u_zonal_padded), 1/30)
     # find max
-    max_ind = np.argmax(power_spec)
+    max_ind = np.argmax(power_spec, axis=0)
     peak_freq = freqs[max_ind]
     period = 1/peak_freq
 
